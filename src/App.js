db708050e5651f08eb820e2c3bcacd2cde035e5a
Garrett Grimm <ggrimm@prudentpublishing.com>
2017-03-27 23:41:42 -0700

    now you can load a MIDI file and view in roll

--- src/App.js
+++ src/App.js
@@ -1,43 +1,79 @@
 import React, { Component } from 'react';
-import './App.css';
+import './css/App.css';
 import Constants from './Constants';
 import Player from './../node_modules/midi-player-js';
-import Mario from './mario-midi.js';
-import Buns from './hot-cross-buns-midi.js';
+import Mario from './midi/mario-midi.js';
+//import Buns from './midi/hot-cross-buns-midi.js';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-                  selectedTrack: 0,
-                  currentTick: 0
-                  };
+    // How wide in pixels a single beat should be.
+    this.beat_pixel_length = 20;
+
+    // Initialize MIDI parser/player
     this.midiPlayer = new Player.Player();
-    this.midiPlayer.loadDataUri(Mario);
     this.midiPlayer.on('playing', function(tick) {
       this.handlePlayTick(tick.tick);
 
     }.bind(this));
+    this.midiPlayer.loadDataUri(Mario);
+
     //this.midiPlayer.play();
-    this.midiEvents = this.midiPlayer.events[this.state.selectedTrack];
-    this.beat_pixel_length = 20;
-    this.tick_pixel_length = this.beat_pixel_length / this.midiPlayer.division;
-    this.commonValues = {
-      "tickPixelLength" : this.beat_pixel_length / this.midiPlayer.division
-    };
+
+    this.state = {
+                  error:            null,
+                  selectedTrack:    0,
+                  currentTick:      0,
+                  midiEvents:       this.midiPlayer.events[0],
+                  tickPixelLength:  this.beat_pixel_length / this.midiPlayer.division
+                  };
+
+    this.settingState = false;
+
+    // Bind methods to this
     this.handleTrackChange = this.handleTrackChange.bind(this);
+    this.handleFileChange = this.handleFileChange.bind(this);
     this.handlePlayTick = this.handlePlayTick.bind(this);
   }
 
   handleTrackChange(selectedTrack) {
-    this.setState({selectedTrack: selectedTrack});
-    this.setState({currentTick:this.state.currentTick+10});
-    this.midiEvents = this.midiPlayer.events[selectedTrack];
+    this.setState({
+                    selectedTrack:  selectedTrack,
+                    midiEvents:     this.midiPlayer.events[selectedTrack],
+                    currentTick:    this.state.currentTick + 10
+                  });
+  }
+
+  handleFileChange(file) {
+    var reader  = new FileReader();
+
+    reader.onload = function(e) {
+      try {
+        this.midiPlayer.loadArrayBuffer(e.target.result);
+        this.setState({
+                        error:            null, 
+                        midiEvents:       this.midiPlayer.events[0],
+                        tickPixelLength:  this.beat_pixel_length / this.midiPlayer.division
+                      });
+
+      } catch(e) {
+        this.setState({error: e});
+      }
+      
+    }.bind(this);
+
+    reader.readAsArrayBuffer(file);
   }
 
   handlePlayTick(tick) {
-    console.log(tick)
-    this.setState({currentTick:tick});
+    //console.log(tick)
+    if (!this.settingState) {
+      this.settingState = true;
+      this.setState({currentTick: tick}, function() {
+        this.settingState = false;
+      }.bind(this));
+    }
   }
 
   render() {
@@ -46,10 +82,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h2>Welcome to React Piano Roll</h2>
+          <ChooseFile onFileChange={this.handleFileChange} appState={this.state} />
           <SelectTrack midiPlayer={this.midiPlayer} onTrackChange={this.handleTrackChange} />
         </div>
         <Piano />
-        <Roll midiEvents={this.midiEvents} commonValues={this.commonValues} appState={this.state} />
+        <Roll appState={this.state} />
       </div>
     );
   }
@@ -58,7 +95,7 @@ class App extends Component {
 class Playhead extends Component {
   render() {
     return (
-      <div className="Playhead" style={{left:this.props.appState.currentTick}}></div>
+      <div className="Playhead" style={{left: this.props.appState.currentTick}}></div>
     );
   }
 }
@@ -92,7 +129,7 @@ class Roll extends Component {
     var rows = [];
 
     Constants.NOTES.forEach(function(noteObject) {
-      rows.push(<Row key={noteObject.midiNumber} midiNumber={noteObject.midiNumber} midiEvents={this.props.midiEvents} commonValues={this.props.commonValues} />);
+      rows.push(<Row key={noteObject.midiNumber} midiNumber={noteObject.midiNumber} midiEvents={this.props.appState.midiEvents} appState={this.props.appState} />);
     }.bind(this));
 
     return (
@@ -114,19 +151,12 @@ class Row extends Component {
       return false;
     }.bind(this));
 
-
-    /** 
-    * Process of building squares:
-    * Mark the start of a note, but
-    *
-    */
-
     var squares = midiEvents.map(function(event, index) {
       if (event.name === 'Note on') {
         // The next event should either be a "Note on" with 0 velocity (running status), or a "Note off".
         if (event.velocity > 0 && midiEvents[index + 1]) {
             let width = midiEvents[index + 1].delta;
-            return <Square key={index} width={width * this.props.commonValues.tickPixelLength} left={event.tick * this.props.commonValues.tickPixelLength} />
+            return <Square key={index} width={width * this.props.appState.tickPixelLength} left={event.tick * this.props.appState.tickPixelLength} />
         }
 
         return null;
@@ -145,7 +175,29 @@ class Row extends Component {
 class Square extends Component {
   render() {
     return (
-      <div className="Square" style={{"width" : this.props.width   + "px", "left" : this.props.left   + "px"}}></div>
+      <div className="Square" style={{"width": this.props.width   + "px", "left": this.props.left   + "px"}}></div>
+    );
+  }
+}
+
+class ChooseFile extends Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.props.onFileChange(event.target.files[0]);
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="file" onChange={this.handleChange} />
+        {this.props.appState.error &&
+          <p>{this.props.appState.error}</p>
+        }
+      </div>
     );
   }
 }
@@ -164,7 +216,7 @@ class SelectTrack extends Component {
     var options = this.props.midiPlayer.tracks.map((element, index) => <option key={index}>{index}</option>);
 
     return (
-      <p><label>Select Track</label><select onChange={this.handleChange} style={{"color" : "black"}}>{options}</select></p>
+      <p><label>Select Track</label><select onChange={this.handleChange} style={{"color": "#000"}}>{options}</select></p>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './../node_modules/skeleton-css/css/normalize.css';
 import './../node_modules/skeleton-css/css/skeleton.css';
-import './index.css';
+import './css/index.css';
 
 ReactDOM.render(
   <App />,
